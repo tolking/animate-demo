9a227bb66c03bf147575e3ded09397f7babd181c
tolking <qw13131wang@gmail.com>
2022-08-28 14:49:55 +0800

    fix: the dependencies error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { resolve } from 'path'
+// import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@vueuse/core': resolve(__dirname, '../vueuse/packages/core/index.ts'),
-      '@vueuse/shared': resolve(__dirname, '../vueuse/packages/shared/index.ts'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@vueuse/core': resolve(__dirname, '../vueuse/packages/core/index.ts'),
+  //     '@vueuse/shared': resolve(__dirname, '../vueuse/packages/shared/index.ts'),
+  //   },
+  // },
   optimizeDeps: {
-    exclude: ['vue-demi']
+    exclude: ['vue-demi'],
   },
   build: {
     outDir: 'docs',
